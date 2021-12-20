614d55a30f1ba375d39e241ba071edddf7eb571f
sungwoo choi <sqlstyle@kakao.com>
2021-12-20 23:06:01 +0900

    폴더 구조 변경 및 npm install

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const MongoClient
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,6 +27,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
