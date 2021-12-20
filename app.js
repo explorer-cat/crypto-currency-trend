81440656a43828cb08a362d75bf0fd453c3027e6
sungwoo choi <sqlstyle@kakao.com>
2021-12-20 23:23:02 +0900

    local database mongoose connect!

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+//local mongoose connect!
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017",{
+    dbName: "company-trend",
+  }).then(() => console.log(`mongoDB connection`)).catch((err) => console.error(err));
 
+
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
