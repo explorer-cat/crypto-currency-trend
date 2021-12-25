a6e188569d8acdc985b3393c7be9c406bcec43eb
sungwoo choi <sqlstyle@naver.com>
2021-12-25 22:16:15 +0900

    setChangeToColor 함수 추가 BTC,ETH,XRP 가격 변동률 완성

--- public/javascripts/constants/dashboard/DashboardDataInfo.js
+++ public/javascripts/constants/dashboard/DashboardDataInfo.js
@@ -4,128 +4,13 @@
  * @created 최성우 2021-09 00:00 최초 개발
  */
 
-// function initCompanyInfo() {
-//     setMemberInfo();
-// }
-
-// function setMemberInfo() {
-//     var ctx = document.getElementById('working_member');
-
-//     var data = [12,19,3,5,2,3];
-//     var myChart = new Chart(ctx, {
-//         type: 'doughnut',
-//         data: {
-//        //     labels: ["출근전", "휴식", "외근", "근무중", "퇴근", "출장"],
-//             datasets: [{
-//                 label: '# of Votes',
-//                 data: data,
-//                 backgroundColor: [
-//                     '#2E2EFE',
-//                     '#04B4AE',
-//                     '#BF00FF',
-//                     '#BDBDBD',
-//                     '#0B615E',
-//                     '#868A08'
-//                 ],
-//                 borderWidth: 1
-//             }]
-//         },
-//         options: {
-//             responsive: false,
-//             // scales: {
-//             //     yAxes: [{
-//             //         ticks: {
-//             //             beginAtZero: true
-//             //         }
-//             //     }]
-//             // },
-//         }
-//     });
-
-//     var result = document.getElementById('work_chart_result');
-
-// }
-
-// let socekt;
-
-//     //업비트 비트코인 가격 정보
-//     async function getUpbitBitCoinInfo() {
-//         let getURL = ""
-//         let response;
-//         try {
-//             if(socket != undefined){
-//                 socket.close();
-//             }
-            
-//             socket = new WebSocket("wss://api.upbit.com/websocket/v1");
-//             socket.binaryType = 'arraybuffer';
-        
-//             socket.onopen 	= function(e){ 
-//                 filterRequest(`
-//                 [
-//                     {"ticket": "UNIQUE_TICKET"},
-//                     {"type" : "ticker","codes":["KRW-BTC"]},		// "KRW-BTC","KRW-ETH" -> parsing
-//                     {"type" : "orderbook","codes":["KRW-BTC"]},
-//                     {"type" : "trade","codes":["KRW-BTC"]},
-//                 ]`); 
-//             }
-//             socket.onclose 	= function(e){ 
-//                 socket = undefined; 
-//             }
-//             socket.onmessage= function(e){ 
-//                 var enc = new TextDecoder("utf-8");
-//                 var arr = new Uint8Array(e.data);
-//                 var str_d = enc.decode(arr);
-//                 var d = JSON.parse(str_d);
-
-//                 if(d.type == "ticker") { // 현재가 데이터
-//                 // TODO
-//                 }
-//                 if(d.type == "orderbook") { // 호가 데이터
-//                 // TODO
-//                 }
-//                 if(d.type == "trade") { // 체결 데이터
-//                 // TODO
-//                 }
-//                 console.log(d)
-//             }
-//             // let response = await axios({
-//             //     headers : {Accept : 'application/json'},
-//             //     url: 'https://api.upbit.com/v1/ticker?markets=KRW-BTC',
-//             //     method: 'get',
-//             //   });
-//               return callback(response.data[0]);
-//         } catch (e) {
-//             console.error('업비트 정보 불러오기 에러',e);
-//             response = {err:true}
-//         }
-//     }
-
-
-
-// // 웹소켓 연결 해제
-// function closeWS() {
-// 	if(socket != undefined){
-// 		socket.close();
-// 		socket = undefined;
-// 	}	
-// }
-
-// // 웹소켓 요청
-// function filterRequest(filter) {
-// 	if(socket == undefined){
-// 		alert('no connect exists');
-// 		return;
-// 	}
-// 	socket.send(filter);
-// }
-
-// getUpbitBitCoinInfo();
 
 var socket; // 소켓
 
 // 웹소켓 연결
-function getUpbitBitCoinInfo(callback) {
+function getUpbitCoinInfo(callback) {
+
+
 	if(socket != undefined){
 		socket.close();
 	}
@@ -136,9 +21,7 @@ function getUpbitBitCoinInfo(callback) {
 	socket.onopen 	= function(e){ 
 		filterRequest(`[
             {"ticket":"UNIQUE_TICKET"},
-			{"type":"ticker","codes":["KRW-BTC"]},
-			{"type":"orderbook","codes":["KRW-BTC"]},
-			{"type":"trade","codes":["KRW-BTC"]}]`); 
+			{"type":"ticker","codes":["KRW-BTC","KRW-ETH","KRW-XRP"]}]`); 
 	}
 	socket.onclose 	= function(e){ 
 		socket = undefined; 
@@ -149,15 +32,18 @@ function getUpbitBitCoinInfo(callback) {
 		let str_d = enc.decode(arr);
 		let response = JSON.parse(str_d);
 
+
         let result = {
+            code : response.code,
             trade_price : response.trade_price, //현재가
             change : response.change, //RISE : 상승  EVEN : 보합  FALL : 하락
-            signed_change_price : response.signed_change_price, //전일 대비 등락율
-            acc_trade_volume_24h : response.acc_trade_volume_24h  //24시간 누적 거래량
+            signed_change_price : response.signed_change_price, //전일 대비 등락가격
+            acc_trade_volume_24h : response.acc_trade_volume_24h,  //24시간 누적 거래량
+            signed_change_rate : response.signed_change_rate * 100 //부호 있는 전일 등락률
         }
             
             if(response.type === 'ticker') {
-                console.log('ticker', result)
+              //  console.log(result)
                 return callback(result);
             }
 		}
@@ -179,5 +65,22 @@ function filterRequest(filter) {
 	}
 	socket.send(filter);
 }
+
+//전일 대비 상승,보합,하락에 따라 document의 색상을 변경해주는 함수
+function setChangeToColor(code,change,el) {
+    if(change === "RISE") {
+        for (const property in el) {
+            el[property].classList.add("up_green_color")
+            el[property].classList.remove("down_red_color")
+          }
+    } else {
+        for (const property in el) {
+            el[property].classList.add("down_red_color")
+            el[property].classList.remove("up_green_color")
+          }
+    } 
+}
+
+
 
 
--- public/javascripts/constants/dashboard/LoadDashboardInfo.js
+++ public/javascripts/constants/dashboard/LoadDashboardInfo.js
@@ -4,13 +4,52 @@
  * @created 최성우 2021-09 00:00 최초 개발
  */
 
+
+
 window.onload = async function () {
+    //가격 , 등락률 , 등락가격
+    let upbitBTC = {
+        "KRW" : document.getElementById('upbit_BTC_krw'),
+        "RATE" : document.getElementById('upbit_BTC_signed_change_rate'),
+    }
+
+    let upbitETH = {
+        "KRW" : document.getElementById('upbit_ETH_krw'),
+        "RATE" : document.getElementById('upbit_ETH_signed_change_rate'),
+    }
+
+    let upbitXRP = {
+        "KRW" : document.getElementById('upbit_XRP_krw'),
+        "RATE" : document.getElementById('upbit_XRP_signed_change_rate'),
+    }
+
+
 
     //가공된 업비트 비트코인 정보를 받아옵니다.
-    getUpbitBitCoinInfo(async function (result) {
-        document.getElementById('upbit_bitcoin_krw').innerHTML = result.trade_price;
-        document.getElementById('upbit_bitcoin_signed_change_price').innerHTML = result.signed_change_price;
+    getUpbitCoinInfo(async function (result) {
+        //상승 보합 하락 기준에 따라 view를 다르게 지정합니다.
+        //result.change -> RISE : 상승  EVEN : 보합  FALL : 하락
+
+        switch(result.code)  {
+            case "KRW-BTC":
+                upbitBTC.KRW.innerHTML = `${result.trade_price.toLocaleString()}원`;
+                upbitBTC.RATE.innerHTML = `전일대비 ${result.signed_change_rate.toFixed(2)}%`;
+                setChangeToColor(result.code, result.change, upbitBTC);
+            break;
+            case "KRW-ETH":
+                document.getElementById('upbit_ETH_krw').innerHTML = `${result.trade_price.toLocaleString()}원`;
+                document.getElementById('upbit_ETH_signed_change_rate').innerHTML = `전일대비 ${result.signed_change_rate.toFixed(2)}%`;
+                setChangeToColor(result.code, result.change, upbitETH);
+                break
+            case "KRW-XRP":
+                document.getElementById('upbit_XRP_krw').innerHTML = `${result.trade_price.toLocaleString()}원`;
+                document.getElementById('upbit_XRP_signed_change_rate').innerHTML = `전일대비 ${result.signed_change_rate.toFixed(2)}%`;
+                setChangeToColor(result.code, result.change, upbitXRP);
+                break;
+        }
+
+
+
+
     });
-
-
 }
