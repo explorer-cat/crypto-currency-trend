c99488be2af4afffa4243de1b800f1e5b229aca4
sungwoo choi <sqlstyle@naver.com>
2021-12-25 20:00:11 +0900

    upbit bitcoin 시세조회 기능 추가

--- public/javascripts/constants/dashboard/LoadDashboardInfo.js
+++ public/javascripts/constants/dashboard/LoadDashboardInfo.js
@@ -4,6 +4,13 @@
  * @created 최성우 2021-09 00:00 최초 개발
  */
 
-window.onload = function () {
-    initCompanyInfo();
+window.onload = async function () {
+
+    //가공된 업비트 비트코인 정보를 받아옵니다.
+    getUpbitBitCoinInfo(async function (result) {
+        document.getElementById('upbit_bitcoin_krw').innerHTML = result.trade_price;
+        document.getElementById('upbit_bitcoin_signed_change_price').innerHTML = result.signed_change_price;
+    });
+
+
 }
