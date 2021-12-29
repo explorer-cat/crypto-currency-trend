756e55df151739cee6eb077c6571c652e723b54c
sungwoo choi <sqlstyle@naver.com>
2021-12-29 20:58:02 +0900

    업비트 실시간 매수매도 체결금액 함수 구현

--- config.js
+++ config.js
@@ -1,18 +1,1 @@
 //databaseconnection
-
-module.exports = {
-    DataBaseConnect : {
-        Host : 'localhost:3306',
-        User : 'root',
-        Pass : 'cch1676816@',
-        Database : 'coin-trend',
-        connectionLimit : 10,
-    },
-    //업비트 API 세팅
-    upbitAPI : {
-            "access_key" : "KuKtdvxxNxRQdfh1r7AxsjYx5kNMq0XxXByoAeXa",
-            "nonce" : "test",
-            "query_hash" : "",
-            "query_hash_alg" : "SHA512"
-    }
-}
